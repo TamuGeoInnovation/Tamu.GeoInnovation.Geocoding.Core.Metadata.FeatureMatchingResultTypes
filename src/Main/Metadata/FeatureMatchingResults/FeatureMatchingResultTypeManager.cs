71ce8836a752ddc87b147221c6eb8ac560b81ad9
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-18 16:59:12 -0500

    Initial Commit

---
+++ src/Main/Metadata/FeatureMatchingResults/FeatureMatchingResultTypeManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+
+namespace USC.GISResearchLab.Geocoding.Core.Metadata.FeatureMatchingResults
+{
+
+    [Flags]
+    public enum FeatureMatchingResultType
+    {
+        Unknown = 1,
+        Success = 2,
+        Ambiguous = 4,
+        BrokenTie = 8,
+        Composite = 16,
+        Nearby = 32,
+        LessThanMinimumScore = 64,
+        InvalidFeature = 128,
+        NullFeature = 256,
+        Unmatchable = 512,
+        ExceptionOccurred = 1024,
+    }
+
+    public enum FeatureMatchingGeographyType
+    {
+        Unknown,
+        GPS,
+        BuildingCentroid,
+        BuildingDoor,
+        Parcel,
+        StreetSegment,
+        StreetIntersection,
+        StreetCentroid,
+        USPSZipPlus5,
+        USPSZipPlus4,
+        USPSZipPlus3,
+        USPSZipPlus2,
+        USPSZipPlus1,
+        USPSZip,
+        ZCTAPlus5,
+        ZCTAPlus4,
+        ZCTAPlus3,
+        ZCTAPlus2,
+        ZCTAPlus1,
+        ZCTA,
+        City,
+        ConsolidatedCity,
+        MinorCivilDivision,
+        CountySubRegion,
+        County,
+        State,
+        Country,
+        Unmatchable,
+    }
+
+    
+}
